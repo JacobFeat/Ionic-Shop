55bdfd0f94863f2ea1434c3076df2db78a09f83b
JacobFeat <jakubk198@gmail.com>
2023-02-06 12:20:06 +0100

    set  custom back icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,11 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot({ backButtonIcon: './assets/icon/arrow-left.svg' }),
+    AppRoutingModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
