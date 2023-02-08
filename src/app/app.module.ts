4a10b6a7289d93d385cc26f4c05baa4ae12171c6
JacobFeat <jakubk198@gmail.com>
2023-02-08 13:42:50 +0100

    add localePL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -6,6 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import '@angular/common/locales/global/pl';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,7 +15,14 @@ import { AppRoutingModule } from './app-routing.module';
     IonicModule.forRoot({ backButtonIcon: './assets/icon/arrow-left.svg' }),
     AppRoutingModule,
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: LOCALE_ID, useValue: 'pl-PL' },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'PLN',
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
